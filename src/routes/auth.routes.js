12ba4485980f23ea9ae660d65745b3c5293545fd
SadikshyaAdhikari <sadikshyaadhikari2004@gmail.com>
2026-01-13 16:59:49 +0545

    feat(auth):delete functionality added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { registerUser } from '../controllers/auth.controllers.js';
 import { loginUser } from '../controllers/auth.controllers.js';
+import { deleteUser } from '../controllers/auth.controllers.js';
 
 const router = express.Router();
 
@@ -9,6 +10,9 @@ router.post('/register', registerUser);
 
 //Login route
 router.post('/login', loginUser);
+
+//Delete user route
+router.delete('/delete/:id', deleteUser);
 
 // module.exports = router;
 export default router;
