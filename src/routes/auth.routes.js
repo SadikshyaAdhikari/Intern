772062f56e788c43385ac4523b8af2c6d96ac15f
SadikshyaAdhikari <sadikshyaadhikari2004@gmail.com>
2026-01-18 10:25:06 +0545

    feat(auth):feature to view only own's detail added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,8 @@ import { registerUser } from '../controllers/auth.controllers.js';
 import { loginUser } from '../controllers/auth.controllers.js';
 import { deleteUser } from '../controllers/auth.controllers.js';
 import { adminOnly } from '../middleware/adminOnly.js';
+import { viewMyDetails } from '../controllers/auth.controllers.js';
+import { userOnly } from '../middleware/userOnly.js';
 
 const router = express.Router();
 
@@ -11,12 +13,19 @@ router.post('/register', registerUser);
 
 //Login route
 router.post('/login', loginUser);
+
+//Delete user route
+router.delete('/delete/:id', adminOnly, deleteUser);
+
+
 //view my details
+// router.post('/me', viewMyDetails);
+router.get('/me/:id', userOnly, viewMyDetails);
+
+
 // /me
 //whoeever calls it should get its informtation(token)
 //if token expired, it should not give access
-//Delete user route
-router.delete('/delete/:id', adminOnly, deleteUser);
 
 // module.exports = router;
 export default router;
